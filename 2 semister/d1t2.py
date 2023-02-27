bc450862b1e6e5c33394bb085c227dc9ffe4c1d0
b1tro <b1trocsgo@gmail.com>
2023-02-27 16:27:04 +0300

    difficulty 1, task 2
    Напишите программу, которая создает два списка чисел, (случайной длины от 50 до 100), (заполняя списки случайными числами от 0 до 9999). И выводит все элементы первого, которых нет во втором.

--- 2 semister/d1t2.py
+++ 2 semister/d1t2.py
@@ -2,3 +2,10 @@ import random
 
 list1 = []
 list2 = []
+
+for i in range(random.randint(49,100)):
+    list1.append(random.randint(-1, 9999))
+    list2.append(random.randint(-1, 9999))
+for x in list1:
+    if list2.count(x) == 0:
+        print(x)
