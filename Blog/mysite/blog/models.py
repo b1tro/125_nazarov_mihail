e0eda628b48a194982c10a5346dd36a78538e38f
b1tro <b1trocsgo@gmail.com>
2023-04-25 21:15:59 +0300

    Blog v 1.0

--- Blog/mysite/blog/models.py
+++ Blog/mysite/blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 class PublishedManager(models.Manager):
     def get_queryset(self):
@@ -20,16 +21,18 @@ class Post(models.Model):
     updated = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=2, choices= Status.choices, default=Status.DRAFT)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
-    def __str__(self):
-        return self.title
 
     objects = models.Manager()
     published = PublishedManager()
-
     class Meta:
         ordering = ['-publish']
         indexes = [
             models.Index(fields=['-publish'])
         ]
 
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail', args=[self.id])
 
--- Blog/mysite/blog/views.py
+++ Blog/mysite/blog/views.py
@@ -8,6 +8,6 @@ def post_list(request):
                   {'posts': posts})
 
 def post_detail(request,id):
-    get_object_or_404(Post, id=id, status=Post.Status.PUBLISHED)
+    post = get_object_or_404(Post, id=id, status=Post.Status.PUBLISHED)
     return render(request, 'blog/post/detail.html',
                   {'post': post})
