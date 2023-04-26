741272cd6bd96739d34b95e5c1c0132a52ea191f
b1tro <b1trocsgo@gmail.com>
2023-04-26 20:47:30 +0300

    blog v 1.1 + leetcode

---
+++ LeetCode/121. Best Time to Buy and Sell Stock.py
@@ -0,0 +1,13 @@
+prices = [7,1,5,3,6,4]
+
+def maxProfit(prices):
+    if len(prices) < 2:
+        return 0
+    min_price = prices[0]
+    max_profit = 0
+    for price in prices:
+        if price < min_price:
+            min_price = price
+        elif price - min_price > max_profit:
+            max_profit = price - min_price
+    return max_profit
